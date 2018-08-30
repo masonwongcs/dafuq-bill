c2cc067360b99bea76135996233eed1464b883a7
Kevin Kam <kevin.kam@orion.co.com>
2018-08-30 15:51:02 +0800

    updated expo

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <Text>Hello World!</Text>
+        <Text>Hello World!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -19,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
