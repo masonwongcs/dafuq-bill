47b27fae13161b7e74dc0f8b7f9ff66ae8516d33
Kevin Kam <kevin.kam@orion.co.com>
2018-08-30 15:02:22 +0800

    implment typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+          <Text>Hello World!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
