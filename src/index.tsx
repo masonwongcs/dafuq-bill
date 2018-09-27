99c0d7c692b5882a93801f19501e059791a7650b
Kevin Kam <k040691@icloud.com>
2018-09-27 19:44:16 +0800

    added redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
 import { LocaleProvider } from 'antd-mobile-rn'
 import enUS from 'antd-mobile-rn/lib/locale-provider/en_US'
 import { Provider } from 'react-redux'
-import { history, store } from './store'
+import { history, store, persistor } from './store'
 import { ConnectedRouter } from 'connected-react-router'
 import Container from './components/Container'
 
@@ -15,9 +16,11 @@ export default class App extends React.Component<AppProps, AppState> {
     return (
       <LocaleProvider locale={enUS}>
         <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <Container />
-          </ConnectedRouter>
+          <PersistGate persistor={persistor}>
+            <ConnectedRouter history={history}>
+              <Container />
+            </ConnectedRouter>
+          </PersistGate>
         </Provider>
       </LocaleProvider>
     )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,25 @@
-import {createStore, applyMiddleware, compose} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createMemoryHistory } from 'history'
 import { routerMiddleware, connectRouter } from 'connected-react-router'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
 import Reducer from '../reducers'
 
-let Store = history=>{
-  return createStore(connectRouter(history)(Reducer), compose(applyMiddleware(routerMiddleware(history))))
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, Reducer)
+
+let Store = history => {
+  return createStore(connectRouter(history)(persistedReducer), compose(applyMiddleware(routerMiddleware(history))))
 }
 
 const history = createMemoryHistory()
 const store = Store(history)
-const { dispatch, getState} = store
+const persistor = persistStore(store)
+const { dispatch, getState } = store
 
-export {history, store, dispatch, getState}
+export { history, store, dispatch, getState, persistor }
